7288322776f3f713061b6ce44cd0c36293ea862b
Sterling <sterlingcobb@gmail.com>
2023-03-04 01:08:48 -0700

    add lens context
    Former-commit-id: 4b00153a1c7589864effc45f2de1f562b83014f6

--- App.jsx
+++ App.jsx
@@ -5,15 +5,18 @@ import { LivepeerConfig } from "@livepeer/react-native";
 import LPClient from "./clients/livepeer";
 import { ApolloProvider } from "@apollo/client";
 import APClient from "./clients/apollo";
+import { LensProvider } from "./LensContext";
 
 export default function App() {
   return (
-    <WalletConnectProvider>
-      <ApolloProvider client={APClient}>
-        <LivepeerConfig client={LPClient}>
-          <Routes />
-        </LivepeerConfig>
-      </ApolloProvider>
-    </WalletConnectProvider>
+    <LensProvider>
+      <WalletConnectProvider>
+        <ApolloProvider client={APClient}>
+          <LivepeerConfig client={LPClient}>
+            <Routes />
+          </LivepeerConfig>
+        </ApolloProvider>
+      </WalletConnectProvider>
+    </LensProvider>
   );
 }
