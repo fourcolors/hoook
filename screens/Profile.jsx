2570c38f389d76c4749617794581e664f186bc8e
Sterling <sterlingcobb@gmail.com>
2023-03-04 14:05:09 -0700

    update profile page
    Former-commit-id: 105f424a83ca85f26c1b8fd5351f4c85b53717e5

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { View } from "react-native";
 import { Profile } from "@lens-protocol/react-native-lens-ui-kit";
 
 export default function ProfileScreen() {
-  return (
-    <View>
-      <Profile profile={{ handle: "web3forbasicbs.lens" }} />
-    </View>
-  );
+  return <Profile handle="web3forbasicbs.lens" />;
 }
