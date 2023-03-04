1fccebbc154177f9a96ea4f33aad8af7cc6acf75
Sterling <sterlingcobb@gmail.com>
2023-03-03 21:26:07 -0700

    add assets and style up bottom bar
    Former-commit-id: 54a9ba9c13efb8e5b71ab98ec40cbf87278dceec

--- components/MusicSelector.jsx
+++ components/MusicSelector.jsx
@@ -1,24 +1,146 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  PanResponder,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import { Audio } from "expo-av";
+// define the array of songs with their information
+const songs = [
+  {
+    albumArt: require("./a1.png"),
+    songTitle: "To the end",
+    artistName: "Wave Hart",
+    audioFile: require("./ToTheEnd-WavHart.mp3"),
+  },
+  {
+    albumArt: require("./a2.png"),
+    songTitle: "Song 2",
+    artistName: "Artist 2",
+    audioFile: require("./wavhart.mp3"),
+  },
+  {
+    albumArt: require("./a3.png"),
+    songTitle: "Song 3",
+    artistName: "Artist 3",
+    audioFile: require("./wavhart.mp3"),
+  },
+  {
+    albumArt: require("./a4.png"),
+    songTitle: "Song 4",
+    artistName: "Artist 4",
+    audioFile: require("./wavhart.mp3"),
+  },
+];
 
-const MusicBar = ({ albumArt, songTitle, artistName }) => {
+const { height: WINDOW_HEIGHT, width: WINDOW_WIDTH } = Dimensions.get("window");
+
+const MusicBar = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [sound, setSound] = useState(null);
+  const [songIndex, setSongIndex] = useState(0);
+  const [songTitleOffset, setSongTitleOffset] = useState(0);
 
-  const handlePlayPause = () => {
-    setIsPlaying(!isPlaying);
+  useEffect(() => {
+    // stop
+    return () => {
+      if (sound !== null) {
+        sound.unloadAsync();
+      }
+    };
+  }, [songIndex]);
+
+  const handlePlayPause = async () => {
+    if (sound !== null) {
+      await sound.unloadAsync();
+      setSound(null);
+    }
+
+    if (!isPlaying) {
+      const { sound: newSound } = await Audio.Sound.createAsync(
+        songs[songIndex].audioFile
+      );
+      setSound(newSound);
+
+      await newSound.playAsync();
+      await newSound.setVolumeAsync(1.0);
+      setIsPlaying(true);
+    } else {
+      setIsPlaying(false);
+    }
   };
+
+  const handleSongTitleSwipe = (evt, gestureState) => {
+    setSongTitleOffset(gestureState.dx);
+  };
+
+  const handleSongTitleSwipeEnd = () => {
+    const songTitleWidth = 150; // adjust this value based on your title font size and length
+    const offset = songTitleOffset / songTitleWidth;
+    let index = songIndex;
+
+    if (offset > 0.5 && songIndex < 3) {
+      index++;
+    } else if (offset < -0.5 && songIndex > 0) {
+      index--;
+    }
+
+    setSongTitleOffset(0);
+    setSongIndex(index);
+  };
+
+  const songTitlePosition = {
+    transform: [{ translateX: songTitleOffset }],
+  };
+
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponderCapture: () => true,
+    onPanResponderMove: (evt, gestureState) => {
+      if (gestureState.dx !== 0) {
+        handleSongTitleSwipe(evt, gestureState);
+      }
+    },
+    onPanResponderRelease: () => {
+      handleSongTitleSwipeEnd();
+    },
+  });
 
   return (
     <View style={styles.container}>
-      <Image source={albumArt} style={styles.albumArt} />
-      <View style={styles.songInfoContainer}>
-        <Text style={styles.songTitle}>{songTitle}</Text>
-        <Text style={styles.artistName}>{artistName}</Text>
-      </View>
+      <FlatList
+        data={songs}
+        horizontal
+        snapToInterval={WINDOW_WIDTH}
+        showsHorizontalScrollIndicator={false}
+        decelerationRate="fast"
+        renderItem={({ item, index }) => {
+          console.log("song", item);
+          return (
+            <View style={styles.songContainer}>
+              <Image source={songs[index].albumArt} style={styles.albumArt} />
+              <View style={styles.songInfoContainer}>
+                <Text style={[styles.songTitle]}>{songs[index].songTitle}</Text>
+                <Text style={[styles.artistName]}>
+                  {songs[index].artistName}
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+        onScroll={(e) => {
+          console.log(e);
+        }}
+      />
+
       <TouchableOpacity onPress={handlePlayPause} style={styles.playButton}>
         <FontAwesome
-          name={isPlaying ? "pause" : "play"}
+          name={isPlaying ? "stop" : "play"}
           size={24}
           color="white"
         />
@@ -30,15 +152,21 @@ const MusicBar = ({ albumArt, songTitle, artistName }) => {
 export default MusicBar;
 
 const styles = StyleSheet.create({
+  songContainer: {
+    width: WINDOW_WIDTH,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingHorizontal: 10,
+  },
   container: {
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#222",
     padding: 10,
-    borderRadius: 10,
     height: 60,
-    width: "90%",
-    marginVertical: 10,
+    width: WINDOW_WIDTH,
+    border: 0,
   },
   albumArt: {
     width: 40,
@@ -48,12 +176,13 @@ const styles = StyleSheet.create({
   },
   songInfoContainer: {
     flex: 1,
-    marginRight: 10,
+    marginLeft: 10,
   },
   songTitle: {
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
+    width: 150,
   },
   artistName: {
     color: "gray",
@@ -65,5 +194,6 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "transparent",
   },
 });
